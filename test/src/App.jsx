1c7f06951a88b19b582dcda1e27221f0fa90b60c
Yordan Grigorov <iordan.megaparts@gmail.com>
2025-02-18 17:57:46 +0200

    first react project

---
+++ test/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import HeroArea from './components/HeroArea.jsx'
+import ExpirienceSection from './components/ExperienceSection.jsx'
+import CategorySection from './components/CategorySection.jsx'
+import AboutSection from './components/AboutSection.jsx'
+import FreelanceSection from './components/FreelanceSections.jsx'
+import InfoSection from './components/InfoSection.jsx'
+import ClientSection from './components/ClientSection.jsx'
+import Footer from './components/Footer.jsx'
+import Navigation from './components/Navigation.jsx'
+
+function App() {
+
+
+  return (
+    <>
+      
+      <HeroArea />
+
+      <ExpirienceSection />
+
+      <CategorySection />
+
+      <AboutSection />
+
+      <FreelanceSection />
+
+      <ClientSection />
+
+      <InfoSection />
+
+      <Footer />
+
+    </>
+  )
+}
+
+export default App
