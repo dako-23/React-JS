a379e161e6321cfd56cc32b07581e92ce673f3b2
Yordan Grigorov <iordan.megaparts@gmail.com>
2025-02-24 15:40:30 +0200

    Add groups component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,9 @@
 import PartnersCarousel from "../components/PartnersCarousel.jsx"
 import Content from "../components/Content"
+import Groups from "../components/Groups.jsx"
+import Topics from "../components/Topics.jsx"
 import Description from "../components/Description.jsx"
+import Recommendations from "../components/Recommendations.jsx"
 
 
 export default function Home() {
@@ -9,7 +12,10 @@ export default function Home() {
         <>
             <Content />
             <PartnersCarousel />
+            <Groups />
             <Description />
+            <Topics />
+            <Recommendations />
         </>
     )
 
