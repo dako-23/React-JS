a21b3a16910e2a0f2ca967fd353f9e007011baf0
Yordan Grigorov <iordan.megaparts@gmail.com>
2025-02-24 17:33:01 +0200

    add motion effect on home page generally

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,66 @@
-import PartnersCarousel from "../components/PartnersCarousel.jsx"
-import Content from "../components/Content"
-import Groups from "../components/Groups.jsx"
-import Topics from "../components/Topics.jsx"
-import Description from "../components/Description.jsx"
-import Recommendations from "../components/Recommendations.jsx"
+import { motion } from 'framer-motion';
+import PartnersCarousel from "../components/PartnersCarousel.jsx";
+import Content from "../components/Content";
+import Groups from "../components/Groups.jsx";
+import Topics from "../components/Topics.jsx";
+import Description from "../components/Description.jsx";
+import Recommendations from "../components/Recommendations.jsx";
 
+const childVariants = {
+    hidden: { opacity: 0, y: -50 },
+    visible: {
+        opacity: 1,
+        y: 0,
+        transition: { duration: 1.5, ease: "easeOut" }
+    }
+};
 
 export default function Home() {
-
     return (
         <>
             <Content />
-            <PartnersCarousel />
-            <Groups />
-            <Description />
-            <Topics />
-            <Recommendations />
+            <div className="container mx-auto px-4">
+                <motion.div
+                    variants={childVariants}
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ once: true, amount: 0.2 }}
+                >
+                    <PartnersCarousel />
+                </motion.div>
+                <motion.div
+                    variants={childVariants}
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ once: true, amount: 0.2 }}
+                >
+                    <Groups />
+                </motion.div>
+                <motion.div
+                    variants={childVariants}
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ once: true, amount: 0.2 }}
+                >
+                    <Description />
+                </motion.div>
+                <motion.div
+                    variants={childVariants}
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ once: true, amount: 0.2 }}
+                >
+                    <Topics />
+                </motion.div>
+                <motion.div
+                    variants={childVariants}
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ once: true, amount: 0.2 }}
+                >
+                    <Recommendations />
+                </motion.div>
+            </div>
         </>
-    )
-
-
+    );
 }
