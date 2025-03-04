32273e75e3296312c58a318f7fa24b6d8804e2ed
Yordan Grigorov <iordan.megaparts@gmail.com>
2025-03-04 14:47:26 +0200

    Add image to group list item

--- src/components/GroupListItems.jsx
+++ src/components/GroupListItems.jsx
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom";
+import { motion } from "framer-motion";
 
 export default function GroupListItems({
     _id,
@@ -6,6 +7,7 @@ export default function GroupListItems({
     members,
     isJoined,
     toggleJoin,
+    imageUrl
 }) {
     const navigate = useNavigate();
 
@@ -14,15 +16,27 @@ export default function GroupListItems({
             className="p-5 border border-gray-300 rounded-lg shadow-md cursor-pointer transition hover:shadow-lg bg-gradient-to-r from-lime-100 to-green-200"
             onClick={() => isJoined && navigate(`/groups/${_id}`, { state: { name } })}
         >
+            {imageUrl && (
+                <motion.img
+                    src={imageUrl}
+                    alt={name}
+                    className="w-full h-64 object-cover rounded-lg mb-3"
+                    whileHover={{ scale: 0.9 }}
+                    transition={{ duration: 0.3 }}
+                />
+            )}
             <h3 className="text-lg font-semibold">{name}</h3>
             <p className="text-gray-500">{members} members</p>
-
             <button
                 onClick={(e) => {
                     e.stopPropagation();
                     toggleJoin(_id);
                 }}
-                className={`mt-3 px-4 py-2 rounded-lg font-medium transition  ${isJoined ? "bg-red-700 text-white hover:bg-red-400" : "bg-gray-800 text-white hover:bg-gray-600"
+                className={`mt-3 px-4 py-2 rounded-lg font-medium transition  ${isJoined
+                        ?
+                        "bg-red-700 text-white hover:bg-red-400"
+                        :
+                        "bg-gray-800 text-white hover:bg-gray-600"
                     }`}
             >
                 {isJoined ? "Leave" : "Join"}
--- src/components/GroupsList.jsx
+++ src/components/GroupsList.jsx
@@ -1,15 +1,16 @@
 import React, { useState } from "react";
 import { motion } from "framer-motion";
+import { FiPlusCircle } from "react-icons/fi";
 import GroupListItems from "./GroupListItems.jsx";
 import GroupCreate from "./GroupCreate.jsx";
 
 // test data
 const groupsData = [
-    { _id: 1, name: "Moms in Sofia", members: 134 },
-    { _id: 2, name: "Newborn Care", members: 98 },
-    { _id: 3, name: "Toddler Activities", members: 45 },
-    { _id: 4, name: "Parenting Tips", members: 60 },
-    { _id: 5, name: "Baby Sleep Advice", members: 30 },
+    { _id: 1, name: "Moms in Sofia", members: 134, imageUrl: 'https://media.istockphoto.com/id/1334507973/photo/young-mothers-in-park.jpg?s=612x612&w=0&k=20&c=qtxFBBVlPY2ulzCqFhh5T6zgcAmU3o1oo0TEi0pEn70=' },
+    { _id: 2, name: "Newborn Care", members: 98, imageUrl: 'https://sahyadrihospital.com/wp-content/uploads/2021/04/Newborns-Babies-Diseases.jpg.webp' },
+    { _id: 3, name: "Toddler Activities", members: 45, imageUrl: 'https://images.ctfassets.net/nis7p59xqtob/6GKyAXQqkzTGHLmMIticEh/dc711322c2728f2784b853285d3f3d2a/20220517_LearningCenter_FunFoodActivities.jpg?w=1920&q=50' },
+    { _id: 4, name: "Parenting Tips", members: 60, imageUrl: 'https://toyzone.in/cdn/shop/articles/Good_Parenting_Tips.png?v=1681713625' },
+    { _id: 5, name: "Baby Sleep Advice", members: 30, imageUrl: 'https://www.parents.com/thmb/VnlaN5z8ML552FPcXUkNyAKFxxo=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-670930656-00630aa8bedf4003aaff6490281a91a8.jpg' },
 ];
 
 export default function GroupsList() {
@@ -22,7 +23,9 @@ export default function GroupsList() {
         );
     };
 
-
+    const closeShowCreateGroupHandler = () => {
+        setShowCreateGroup(null)
+    }
 
     return (
         <motion.div
@@ -31,7 +34,17 @@ export default function GroupsList() {
             transition={{ duration: 0.5 }}
             className="max-w-4xl mx-auto py-20"
         >
-            <h2 className="text-2xl font-bold text-gray-800 text-center mb-12">📢 Join a Group</h2>
+            <h2 className="text-2xl font-bold text-gray-800 text-center mb-12">📢 Join the Conversation – Motherhood Made Social!
+                <p className="text-base text-gray-800 text-center max-w-2xl mx-auto mt-2 mb-4 font-medium">
+                    Looking for advice, friendship, or just a good chat?
+                    <span className="font-semibold"> Find your people, connect, and enjoy motherhood together!</span>
+                </p>
+            </h2>
+            <div className="flex justify-center mt-14">
+                <button onClick={setShowCreateGroup} className="flex items-center mb-12 gap-2 px-4 py-3 bg-gray-800 text-white text-lg font-medium rounded-full shadow-md hover:bg-gray-600 transition duration-300">
+                    <FiPlusCircle size={22} /> Create Group
+                </button>
+            </div>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                 {groupsData.map((group) => (
                     <GroupListItems
@@ -39,12 +52,15 @@ export default function GroupsList() {
                         _id={group._id}
                         name={group.name}
                         members={group.members}
+                        imageUrl={group.imageUrl}
                         isJoined={joinedGroups.includes(group._id)}
                         toggleJoin={toggleJoin}
                     />
                 ))}
             </div>
-            {showCreateGroup && <GroupCreate />}
+            {showCreateGroup && <GroupCreate
+                onClose={closeShowCreateGroupHandler}
+            />}
         </motion.div >
     );
 }
