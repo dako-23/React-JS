fa6311fa20f4ce691b72cb0452ab43e95c5418ad
Yordan Grigorov <iordan.megaparts@gmail.com>
2025-03-11 15:49:27 +0200

    Added Lazy Loading with Suspense + Loader Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React, { lazy, Suspense } from "react";
 import ScrollToTop from "./helpers/scrollToTop.js";
 import Home from "./pages/Home"
 import Navbar from "./components/Navbar";
@@ -6,23 +7,27 @@ import Footer from "./components/Footer";
 import Login from "./components/Login.jsx";
 import Register from "./components/Register.jsx";
 import GroupsList from "./components/GroupsList.jsx";
-import GroupChat from "./components/GroupChat.jsx";
+import Loader from "./components/Loader.jsx";
 
+const GroupChat = lazy(() => import('./components/GroupChat.jsx'))
+// import GroupChat from "./components/GroupChat.jsx";
 
 function App() {
   return (
     <Router>
-      <ScrollToTop/>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/users/login" element={<Login />} />
-        <Route path="/users/register" element={<Register />} />
-        <Route path="/groups" element={<GroupsList />} />
-        <Route path="/groups/:id" element={<GroupChat />} />
-        {/* <Route path="*" element={<NotFound />} />  */}
-      </Routes>
-      <Footer />
+      <Suspense fallback={<Loader />}>
+        <ScrollToTop />
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/users/login" element={<Login />} />
+          <Route path="/users/register" element={<Register />} />
+          <Route path="/groups" element={<GroupsList />} />
+          <Route path="/groups/:id" element={<GroupChat />} />
+          {/* <Route path="*" element={<NotFound />} />  */}
+        </Routes>
+        <Footer />
+      </Suspense>
     </Router>
   )
 }
