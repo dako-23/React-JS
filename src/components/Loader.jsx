454ebee4e9df273fca1d3044f609b66630ad050c
Yordan Grigorov <iordan.megaparts@gmail.com>
2025-03-14 17:38:06 +0200

    Update loader
    Add 'Loading...' text

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,8 +2,9 @@ import React from "react";
 
 export default function Loader() {
     return (
-        <div className="flex justify-center items-center">
+        <div className="flex flex-col justify-center items-center">
             <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-b-4 border-gray-800"></div>
+            <p className="mt-2 text-gray-700 font-medium text-lg">Loading...</p>
         </div>
     );
 }
