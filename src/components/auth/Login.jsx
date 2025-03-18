4b58733cb7157294259ffff8f048cab44ed302d4
Yordan Grigorov <iordan.megaparts@gmail.com>
2025-03-18 17:54:59 +0200

    Refactor for using useActionState

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,74 +1,118 @@
 import { motion } from "framer-motion";
 import { Link, useNavigate } from "react-router-dom";
-import authService from "../../services/authService.js";
-import { useState } from "react";
+import { useActionState, useState } from "react";
+import AuthError from "./AuthError.jsx";
+import * as yup from "yup";
+import AuthServerError from "./AuthServerError.jsx";
+import { useLogin } from "../../api/authApi.js";
 
+const validationSchema = yup.object().shape({
+  email: yup.string().email("Invalid email format").required("Email is required"),
+  password: yup.string().min(6, "Password must be at least 6 characters").required("Password is required"),
+});
 
 export default function Login() {
-  const [pending, setPending] = useState(false);
+  const [error, setError] = useState(null);
+  const { login } = useLogin();
   const navigate = useNavigate();
 
-  const [values, setValues] = useState({
-    email: '',
-    password: '',
-  });
-
-  const LoginSubmitHandler = async (e) => {
-    e.preventDefault();
+  const LoginSubmitHandler = async (prevState, formData) => {
+    const values = Object.fromEntries(formData);
 
     try {
-      setPending(true);
+      setError(null);
 
-      await authService.login(values.email, values.password);
+      await validationSchema.validate(values, { abortEarly: false });
+
+      const userData = await login(values.email, values.password);
+
+      localStorage.setItem('accessToken', userData.accessToken);
+      localStorage.setItem("userId", userData._id);
+      localStorage.setItem("username", userData.username);
 
       navigate('/');
 
+      return values;
     } catch (err) {
-      setPending(null)
-      console.log(err);
+
+      if (err.inner) {
+        const errorMessages = {};
+        err.inner.forEach(e => {
+          errorMessages[e.path] = e.message;
+        });
+        setError(errorMessages);
+      } else {
+        setError({ general: err.message });
+      }
+
+      return values;
     }
-  }
-
-  const onChangeHandler = (e) => {
-    setValues(state => ({ ...state, [e.target.name]: e.target.value }));
-
   };
 
+  const [values, loginAction, isPending] = useActionState(LoginSubmitHandler,
+    {
+      email: '',
+      password: ''
+    });
+
   return (
-    <div className="bg-home-pattern h-screen bg-cover bg-center flex items-center justify-center min-h-screen bg-gray-100">
-      <motion.div
-        initial={{ opacity: 0, y: -20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.5 }}
-        className="bg-page-pattern w-full max-w-md p-6 bg-white shadow-lg rounded-2xl"
-      >
-        <h2 className="text-2xl font-semibold text-center h-14 text-gray-800">Login</h2>
-        <motion.form onSubmit={LoginSubmitHandler}
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
+    <>
+      <div className="bg-home-pattern h-screen bg-cover bg-center flex items-center justify-center min-h-screen bg-gray-100">
+        <motion.div
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5 }}
-          className="space-y-4"
+          className="relative bg-page-pattern w-full max-w-md p-6 bg-white shadow-lg rounded-2xl"
         >
-          <input type="email" name="email" placeholder="Email" className="w-full p-3 border rounded-lg" onChange={onChangeHandler} value={values.email} />
-          <input type="password" name="password" placeholder="Password" className="w-full p-3 border rounded-lg" onChange={onChangeHandler} value={values.password} />
-          <motion.button
-            whileHover={{ scale: 1.05 }}
-            whileTap={{ scale: 0.95 }}
-            disabled={pending}
-            value="Login"
-            className={`w-full p-3 ${pending ? 'bg-gray-400 text-white rounded-lg cursor-not-allowed' : 'bg-gray-800 text-white rounded-lg'}`}
+          {error?.general &&
+            <AuthServerError
+              err={error.general}
+              onClose={() => setError(null)}
+            />}
+          <h2 className="text-2xl font-semibold text-center h-14 text-gray-800">Login</h2>
+          <motion.form action={loginAction}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5 }}
+            className="space-y-4"
           >
-            Login
-          </motion.button>
-          <p className="text-center text-sm text-gray-800 mt-4">
-            Don't have an account? <Link to="/users/register" className="text-lime-700 font-semibold">Register</Link>
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              className={`w-full p-3 border rounded-lg ${error?.email ? "border-red-500" : "border-gray-300"}`}
+              defaultValue={values.email} />
+            {error?.email && <AuthError
+              err={error.email}
+            />}
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              className={`w-full p-3 border rounded-lg ${error?.email ? "border-red-500" : "border-gray-300"}`}
+            />
+            {error?.password && <AuthError
+              err={error.password}
+            />}
+            <motion.button
+              whileHover={{ scale: 1.05 }}
+              whileTap={{ scale: 0.95 }}
+              disabled={isPending}
+              value="Login"
+              className={`w-full p-3 ${isPending ? 'bg-gray-400 text-white rounded-lg cursor-not-allowed' : 'bg-gray-800 text-white rounded-lg'}`}
+            >
+              Login
+            </motion.button>
+            <p className="text-center text-sm text-gray-800 mt-4">
+              Don't have an account? <Link to="/users/register" className="text-lime-700 font-semibold">Register</Link>
 
-            <button className='hover:underline ml-1 text-lime-700'></button>
+              <button className='hover:underline ml-1 text-lime-700'></button>
 
-          </p>
-        </motion.form>
-      </motion.div>
-    </div>
+            </p>
+          </motion.form>
+        </motion.div>
+      </div>
+    </>
   );
 }
 
--- src/components/auth/Register.jsx
+++ src/components/auth/Register.jsx
@@ -1,39 +1,57 @@
 import { Link, useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
 import authService from "../../services/authService.js";
-import { useState } from "react";
+import { useState, useActionState } from "react";
+import * as yup from "yup";
+import AuthError from "./AuthError.jsx";
+
+const validationSchema = yup.object().shape({
+    username: yup.string().required("Username is required"),
+    email: yup.string().email("Invalid email format").required("Email is required"),
+    password: yup.string().min(6, "Password must be at least 6 characters").required("Password is required"),
+    rePassword: yup
+        .string()
+        .oneOf([yup.ref("password"), null], "Passwords do not match")
+        .required("Confirm password is required"),
+});
 
 export default function Register() {
-    const [pending, setPending] = useState(false);
+    const [error, setError] = useState(null);
     const navigate = useNavigate();
 
-    const [values, setValues] = useState({
+    const registerSubmitHandler = async (prevState, formData) => {
+        const values = Object.fromEntries(formData);
+        const userData = { username: values.username, email: values.email, password: values.password };
+
+        try {
+            setError(null);
+
+            await validationSchema.validate(values, { abortEarly: false });
+
+            await authService.register(userData);
+            navigate('/');
+            return values;
+        } catch (err) {
+
+            if (err.inner) {
+                const errorMessages = {};
+                err.inner.forEach(e => {
+                    errorMessages[e.path] = e.message;
+                });
+                setError(errorMessages);
+            } else {
+                setError({ general: err.message });
+            }
+            return values;
+        }
+    };
+
+    const [values, registerAction, isPending] = useActionState(registerSubmitHandler, {
         username: '',
         email: '',
         password: '',
         rePassword: '',
     });
-
-
-    const registerSubmitHandler = async (e) => {
-        e.preventDefault();
-        setPending(true);
-
-        const userData = { username: values.username, email: values.email, password: values.password };
-
-        try {
-            await authService.register(userData);
-
-            navigate('/')
-        } catch (err) {
-            setPending(null)
-            console.log(err);
-        }
-    }
-
-    const onChangeHandler = (e) => {
-        setValues(state => ({ ...state, [e.target.name]: e.target.value }));
-    };
 
     return (
         <div className="bg-home-pattern h-screen bg-cover bg-center flex items-center justify-center min-h-screen bg-gray-100">
@@ -43,30 +61,77 @@ export default function Register() {
                 transition={{ duration: 0.5 }}
                 className="bg-page-pattern w-full max-w-md p-6 bg-white shadow-lg rounded-2xl"
             >
+                {error?.general &&
+                    <AuthServerError
+                        err={error.general}
+                        onClose={() => setError(null)}
+                    />}
                 <h2 className="text-2xl font-semibold text-center h-14 text-gray-800">Register</h2>
-                <motion.form method="POST" onSubmit={registerSubmitHandler}
+
+                <motion.form action={registerAction}
                     initial={{ opacity: 0 }}
                     animate={{ opacity: 1 }}
                     transition={{ duration: 0.5 }}
                     className="space-y-4"
                 >
-                    <input type="text" name="username" placeholder="Username" className="w-full p-3 border rounded-lg" onChange={onChangeHandler} value={values.username} />
-                    <input type="email" name="email" placeholder="Email" className="w-full p-3 border rounded-lg" onChange={onChangeHandler} value={values.email} />
-                    <input type="password" name="password" placeholder="Password" className="w-full p-3 border rounded-lg" onChange={onChangeHandler} value={values.password} />
-                    <input type="password" name="rePassword" placeholder="Confirm Password" className="w-full p-3 border rounded-lg" onChange={onChangeHandler} value={values.rePassword} />
+                    <input
+                        type="text"
+                        name="username"
+                        placeholder="Username"
+                        className={`w-full p-3 border rounded-lg ${error?.username ? "border-red-500" : "border-gray-300"}`}
+                        defaultValue={values.username}
+                    />
+                    {error?.username && <AuthError
+                        err={error.username}
+                    />}
+
+                    <input
+                        type="email"
+                        name="email"
+                        placeholder="Email"
+                        className={`w-full p-3 border rounded-lg ${error?.email ? "border-red-500" : "border-gray-300"}`}
+                        defaultValue={values.email}
+                    />
+                    {error?.email && <AuthError
+                        err={error.email}
+                    />}
+
+                    <input
+                        type="password"
+                        name="password"
+                        placeholder="Password"
+                        className={`w-full p-3 border rounded-lg ${error?.password ? "border-red-500" : "border-gray-300"}`}
+                        defaultValue={values.password}
+                    />
+                    {error?.password && <AuthError
+                        err={error.password}
+                    />}
+
+                    <input
+                        type="password"
+                        name="rePassword"
+                        placeholder="Confirm Password"
+                        className={`w-full p-3 border rounded-lg ${error?.rePassword ? "border-red-500" : "border-gray-300"}`}
+                        defaultValue={values.rePassword}
+                    />
+                    {error?.rePassword && <AuthError
+                        err={error.rePassword}
+                    />}
+
                     <motion.button
                         whileHover={{ scale: 1.05 }}
                         whileTap={{ scale: 0.95 }}
-                        className={`w-full p-3 ${pending ? 'bg-gray-400 text-white rounded-lg cursor-not-allowed' : 'bg-gray-800 text-white rounded-lg'}`}
+                        disabled={isPending}
+                        className={`w-full p-3 ${isPending ? 'bg-gray-400 text-white rounded-lg cursor-not-allowed' : 'bg-gray-800 text-white rounded-lg'}`}
                     >
                         Register
                     </motion.button>
-                    <p className="text-center text-sm text-gray-800 mt-4">
-                        Already have an account? <Link to="/users/login" className="text-lime-700 font-semibold">Login</Link>
-                        <button className="text-lime-700 hover:underline ml-1"></button>
-                    </p>
                 </motion.form>
+
+                <p className="text-center text-sm text-gray-800 mt-4">
+                    Already have an account? <Link to="/users/login" className="text-lime-700 font-semibold">Login</Link>
+                </p>
             </motion.div>
         </div>
-    )
+    );
 }
