816c7ed17c5652075437df28718a813b2d70fc54
Yordan Grigorov <iordan.megaparts@gmail.com>
2025-03-18 12:28:54 +0200

    Add request utill function

---
+++ src/utils/request.js
@@ -0,0 +1,36 @@
+const request = async (method, url, data, options = {}) => {
+
+    if (method !== 'GET') {
+        options.method = method;
+    }
+
+    if (data) {
+        options = {
+            ...options,
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data),
+            credentials: 'include'
+        }
+    }
+
+    const response = await fetch(url, options);
+    const responseContentType = response.headers.get('Content-Type');
+    if (!responseContentType) {
+        return;
+    }
+
+    const result = await response.json();
+
+    return result;
+
+};
+
+export default {
+    get: request.bind(null, 'GET'),
+    post: request.bind(null, 'POST'),
+    put: request.bind(null, 'PUT'),
+    delete: request.bind(null, 'DELETE'),
+    baseRequest: request,
+}
