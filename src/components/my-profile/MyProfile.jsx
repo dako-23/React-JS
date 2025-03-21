4a9d275ad9bc0d92c9247245505b7cc6ecf41ead
Yordan Grigorov <iordan.megaparts@gmail.com>
2025-03-21 11:28:49 +0200

    Add my profile
    -Fetch user data
    -Edit profile details
    -Loading state
    -Success notifications

--- src/components/my-profile/MyProfile.jsx
+++ src/components/my-profile/MyProfile.jsx
@@ -1,100 +1,149 @@
-import { motion } from "framer-motion";
-import { useContext, useState } from "react";
-import { UserContext } from "../../contexts/UserContext.jsx";
+import { motion } from 'framer-motion';
+import { useActionState, useContext, useEffect, useState } from 'react';
+import { UserContext } from '../../contexts/UserContext.jsx';
+import { FaUser } from 'react-icons/fa';
+import { useCreateProfileInfo, useGetUser } from '../../api/authApi.js';
+import useFetch from '../../hooks/useFetch.js';
+import { useNavigate } from 'react-router-dom';
+import Loader from '../Loader.jsx';
+import SuccessToast from '../notifications/SuccessToast.jsx';
 
 const MyProfile = () => {
-    const { userId, username, email, isAuth } = useContext(UserContext);
-    const [firstName, setFirstName] = useState("");
-    const [lastName, setLastName] = useState("");
-    const [address, setAddress] = useState("");
+    const { _id: userId, username, email, isAuth, } = useContext(UserContext);
     const [isLocked, setIsLocked] = useState(false);
-    const [profileImage, setProfileImage] = useState(null);
+    const [showNotify, setShowNotify] = useState(false)
+    const navigate = useNavigate()
 
-    const handleImageChange = (event) => {
-        const file = event.target.files[0];
-        if (file) {
-            const imageUrl = URL.createObjectURL(file);
-            setProfileImage(imageUrl);
-        }
-    };
+    const [user, setUser] = useState([]);
+    const { createProfileInfo } = useCreateProfileInfo()
 
-    const handleSave = () => {
-        console.log("Saving user data:", { firstName, lastName, address });
-    };
+    const handleSubmitForm = async (prevState, formData) => {
+        setIsLocked(true)
+        const updateData = Object.fromEntries(formData);
+
+        const result = await createProfileInfo(userId, updateData)
+        setUser(result)
+
+        setTimeout(() => (setIsLocked(false), setShowNotify(true)), 1500);
+        return values
+    }
+
+    const [values, FormAction, isPending] = useActionState(handleSubmitForm, { firstName: '', lastName: '', address: '', imageUrl: '' })
+
+    const { getUser } = useGetUser()
+
+    const { loading, state: fetchedUser } = useFetch(() => getUser(userId));
+
+    useEffect(() => {
+        setUser(fetchedUser);
+        setShowNotify(false)
+    }, [fetchedUser]);
 
     return (
-        <div className="bg-home-pattern h-screen bg-cover bg-center flex items-center justify-center min-h-screen bg-gray-100">
-           <motion.div
-                initial={{ opacity: 0, y: -20 }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{ duration: 0.5 }}
-                className="bg-page-pattern w-full max-w-md p-6 bg-white shadow-lg rounded-2xl"
-            >
-                <h2 className="text-2xl font-semibold text-center h-14 text-gray-800">My Profile</h2>
-                <div className="flex flex-col items-center">
-                    <label htmlFor="profilePic" className="cursor-pointer">
-                        {profileImage ? (
-                            <img src={profileImage} alt="Profile" className="w-24 h-24 rounded-full border border-gray-300 object-cover" />
-                        ) : (
-                            <div className="w-24 h-24 flex items-center text-center justify-center border border-gray-300 rounded-full bg-gray-100 text-gray-500 text-sm">
-                                Upload a photo
-                            </div>
-                        )}
-                    </label>
-                    <input type="file" id="profilePic" className="hidden" onChange={handleImageChange} />
-                    <p className="text-sm text-gray-500 mt-2">Click to change</p>
-                </div>
-                <div className="mt-4">
-                    <label className="block text-sm font-medium text-gray-700">First Name</label>
-                    <input
-                        type="text"
-                        className={`mt-1 w-full p-2 border rounded-lg ${isLocked ? "bg-gray-100 cursor-not-allowed" : ""}`}
-                        placeholder="Enter your first name"
-                        value={firstName}
-                        onChange={(e) => setFirstName(e.target.value)}
-                        readOnly={isLocked}
-                    />
-                </div>
-                <div className="mt-4">
-                    <label className="block text-sm font-medium text-gray-700">Last Name</label>
-                    <input
-                        type="text"
-                        className={`mt-1 w-full p-2 border rounded-lg ${isLocked ? "bg-gray-100 cursor-not-allowed" : ""}`}
-                        placeholder="Enter your last name"
-                        value={lastName}
-                        onChange={(e) => setLastName(e.target.value)}
-                        readOnly={isLocked}
-                    />
-                </div>
-                <div className="mt-4">
-                    <label className="block text-sm font-medium text-gray-700">Email</label>
-                    <input
-                        type="email"
-                        className="mt-1 w-full p-2 border rounded-lg bg-gray-100 cursor-not-allowed"
-                        value={email}
-                        readOnly
-                    />
-                </div>
-                <div className="mt-4">
-                    <label className="block text-sm font-medium text-gray-700">Address</label>
-                    <input
-                        type="text"
-                        className="mt-1 w-full p-2 border rounded-lg"
-                        placeholder="Enter your address"
-                        value={address}
-                        onChange={(e) => setAddress(e.target.value)}
-                    />
-                </div>
-                <div className="mt-6 flex justify-between">
-                    <button className="px-4 py-2 bg-gray-800 text-white rounded-lg hover:bg-gray-600">Cancel</button>
-                    <button 
-                        className="px-4 py-2 bg-lime-600 text-white rounded-lg hover:bg-lime-700" 
-                        onClick={handleSave}
+        <div className="bg-home-pattern h-screen bg-cover bg-center flex items-center justify-center min-h-screen bg-gray-100 relative">
+            {loading
+                ?
+                <Loader />
+                :
+                <>
+                    {showNotify &&
+                        <SuccessToast
+                            message={'Profile update successfully!'}
+                            show={setShowNotify}
+                        />}
+                    <motion.div
+                        initial={{ opacity: 0, y: -20 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        transition={{ duration: 0.5 }}
+                        className="bg-page-pattern w-full max-w-md p-6 bg-white shadow-lg rounded-2xl"
                     >
-                        Save
-                    </button>
-                </div>
-            </motion.div>
+                        <h2 className="text-2xl font-semibold text-center h-14 text-gray-800">My Profile</h2>
+                        <div className="flex flex-col items-center">
+                            <div className="flex items-center justify-center w-24 h-24 rounded-full border-2 border-gray-800 bg-gray-200 shadow-md overflow-hidden">
+                                {user.imageUrl ? (
+                                    <img
+                                        src={user.imageUrl}
+                                        alt="Profile"
+                                        className="w-full h-full object-cover"
+                                    />
+                                ) : (
+                                    <FaUser className="text-gray-600 text-5xl" />
+                                )}
+                            </div>
+                        </div>
+                        <form action={FormAction} className="space-y-4">
+                            <div className="mt-4">
+                                <label className="block text-sm font-medium text-gray-700">First Name</label>
+                                <input
+                                    type="text"
+                                    className={`mt-1 w-full p-2 border rounded-lg ${isLocked ? "bg-gray-100 cursor-not-allowed" : ""}`}
+                                    name='firstName'
+                                    placeholder="Enter your first name"
+                                    defaultValue={user.firstName}
+                                    disabled={isLocked}
+                                />
+                            </div>
+                            <div className="mt-4">
+                                <label className="block text-sm font-medium text-gray-700">Last Name</label>
+                                <input
+                                    type="text"
+                                    className={`mt-1 w-full p-2 border rounded-lg ${isLocked ? "bg-gray-100 cursor-not-allowed" : ""}`}
+                                    name='lastName'
+                                    placeholder="Enter your last name"
+                                    defaultValue={user.lastName}
+                                    disabled={isLocked}
+                                />
+                            </div>
+                            <div className="mt-4">
+                                <label className="block text-sm font-medium text-gray-700">Email</label>
+                                <input
+                                    type="email"
+                                    className="mt-1 w-full p-2 border rounded-lg bg-gray-100 cursor-not-allowed"
+                                    // name='email'
+                                    defaultValue={email}
+                                    disabled={true}
+                                />
+                            </div>
+                            <div className="mt-4">
+                                <label className="block text-sm font-medium text-gray-700">Address</label>
+                                <input
+                                    type="text"
+                                    className={`mt-1 w-full p-2 border rounded-lg ${isLocked ? "bg-gray-100 cursor-not-allowed" : ""}`}
+                                    name='address'
+                                    placeholder="Enter your address"
+                                    defaultValue={user.address}
+                                />
+                            </div>
+                            <div className="mt-4">
+                                <label className="block text-sm font-medium text-gray-700">Upload a photo</label>
+                                <input
+                                    className={`mt-1 w-full p-2 border rounded-lg ${isLocked ? "bg-gray-100 cursor-not-allowed" : ""}`}
+                                    id="imageUrl"
+                                    name="imageUrl"
+                                    type="text"
+                                    placeholder="Image Url"
+                                    defaultValue={user.imageUrl}
+                                    disabled={isLocked}
+                                />
+                            </div>
+                            <div className="mt-6 flex justify-between">
+                                <button
+                                    className="px-4 py-2 bg-gray-800 text-white rounded-lg hover:bg-gray-600"
+                                    onClick={() => navigate('/')}
+                                >
+                                    Cancel
+                                </button>
+                                <button
+                                    className={`px-4 py-2 bg-lime-600 text-white rounded-lg hover:bg-lime-700 ${isLocked && "cursor-not-allowed"}`}
+                                    disabled={isLocked}
+                                >
+                                    Save
+                                </button>
+                            </div>
+                        </form>
+                    </motion.div>
+                </>
+            }
         </div>
     );
 };
