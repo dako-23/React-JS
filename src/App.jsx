74c18c22a99cc7c20362848ff5b536a0416af591
Yordan Grigorov <iordan.megaparts@gmail.com>
2025-03-21 11:31:46 +0200

    Implement lazy loading for my profile & reviews

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,17 @@ import Home from "./components/home/Home.jsx"
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Login from "./components/auth/Login.jsx";
-import MyProfile from "./components/my-profile/MyProfile.jsx";
+// import MyProfile from "./components/my-profile/MyProfile.jsx";
 import Register from "./components/auth/Register.jsx";
 import Loader from "./components/Loader.jsx";
 import { UserProvider } from "./contexts/UserContext.jsx";
-import Reviews from "./components/reviews/Reviews.jsx";
 
-const GroupChat = lazy(() => import('./components/group-chat/GroupChat.jsx'));
 const GroupsList = lazy(() => import('./components/groups/GroupsList.jsx'));
+const GroupChat = lazy(() => import('./components/group-chat/GroupChat.jsx'));
 const AboutPage = lazy(() => import('./components/about/About.jsx'));
+const Reviews = lazy(() => import('./components/reviews/Reviews.jsx'));
+const MyProfile = lazy(() => import('./components/my-profile/MyProfile.jsx'));
+
 
 function App() {
 
