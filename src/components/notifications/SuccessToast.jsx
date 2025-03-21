62c5d94194f769cbe61fbf32f60a62d515d4c371
Yordan Grigorov <iordan.megaparts@gmail.com>
2025-03-21 11:25:52 +0200

    Add success notification

---
+++ src/components/notifications/SuccessToast.jsx
@@ -0,0 +1,28 @@
+import { motion, AnimatePresence } from "framer-motion";
+import { FaCheckCircle } from 'react-icons/fa';
+
+
+export default function SuccessToast({
+    message,
+    show
+}) {
+    setTimeout(() => (show(false)), 1500);
+    return (
+        <AnimatePresence>
+            (
+            <motion.div
+                initial={{ opacity: 0, y: -20 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: -20 }}
+                transition={{ duration: 0.4 }}
+                className="fixed top-16  z-50 bg-lime-600 text-white font-semibold px-4 py-3 rounded-xl shadow-lg flex items-center space-x-2"
+            >
+                <FaCheckCircle className="w-5 h-5 text-white" />
+                <span>{message}</span>
+            </motion.div>
+            )
+        </AnimatePresence>
+    );
+
+
+}
