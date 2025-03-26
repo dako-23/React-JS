2da54279047f7f1fe047b7cf35828d1c86563a53
Yordan Grigorov <iordan.megaparts@gmail.com>
2025-03-26 17:36:33 +0200

    Add post api

---
+++ src/api/postApi.js
@@ -0,0 +1,59 @@
+import { useEffect, useState } from "react";
+import request from "../utils/request.js";
+
+const API_URL = 'https://server-tgjz.onrender.com/news-feed';
+
+export const usePost = () => {
+
+    const create = async (postData) => {
+        return request.post(
+            `${API_URL}/create`,
+            postData
+        );
+    };
+
+    const createComment = async (commentData, postId) => {
+        return request.post(
+            `${API_URL}/${postId}/comment`,
+            commentData
+        );
+    };
+
+    return {
+        create,
+        createComment
+    }
+}
+
+
+export const usePostGetAll = () => {
+
+    const [posts, setPosts] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    const getAll = async () => {
+        try {
+            setLoading(true);
+            const result = await request.get(`${API_URL}`);
+            setPosts(result);
+           
+
+            return result;
+        } catch (error) {
+            console.error("Error fetching groups:", error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        getAll();
+    }, []);
+
+    return {
+        posts,
+        setPosts,
+        getAll,
+        loading
+    };
+};
