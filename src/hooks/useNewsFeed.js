238b886369a88d79018f4e229880a2bc8bdad1a5
Yordan Grigorov <iordan.megaparts@gmail.com>
2025-03-26 17:35:45 +0200

    Add custom hook for news feed

---
+++ src/hooks/useNewsFeed.js
@@ -0,0 +1,117 @@
+import { useActionState, useContext, useState } from "react";
+import { UserContext } from "../contexts/UserContext.jsx";
+import { usePost, usePostGetAll } from "../api/postApi.js";
+import { useToast } from "./useToast.js";
+
+
+export function useNewsFeed() {
+    const [search, setSearch] = useState("");
+    const [sortOption, setSortOption] = useState("newest");
+    const [expandedComments, setExpandedComments] = useState({});
+    const [showPostForm, setShowPostForm] = useState(false);
+
+    const { firstName, lastName, imageUrl: imageUrlAuthor } = useContext(UserContext);
+    const { create, createComment } = usePost()
+    const { posts, loading, setPosts, } = usePostGetAll()
+    const { success } = useToast()
+
+    const filteredPosts = posts
+        .filter((p) => p.content.toLowerCase().includes(search.toLowerCase()))
+        .sort((a, b) => {
+            if (sortOption === "newest") return new Date(b.createdAt) - new Date(a.createdAt);
+            if (sortOption === "oldest") return new Date(a.createdAt) - new Date(b.createdAt);
+            // if (sortOption === "popular") return (b.likes || 0) - (a.likes || 0);
+        });
+
+    const toggleComments = (postId) => {
+        setExpandedComments((prev) => ({
+            [postId]: !prev[postId]
+        }));
+    };
+
+    const handleSubmitPost = async (prevState, formData) => {
+
+        const values = Object.fromEntries(formData);
+        const postData = {
+            ...values,
+            firstName,
+            lastName,
+            imageUrlAuthor
+        }
+
+        try {
+            const newPost = await create(postData);
+
+            setPosts((state) => [newPost, ...state]);
+
+            setShowPostForm(false);
+            success('Post created successfully!');
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    const [postValues, postAction, isPostPending] = useActionState(handleSubmitPost, {
+        content: "",
+        imageUrl: "",
+    });
+
+    const handleSubmitComment = async (prevState, formData) => {
+
+        const values = Object.fromEntries(formData);
+        const commentData = {
+            ...values,
+            firstName,
+            lastName,
+            imageUrlComment: imageUrlAuthor
+        }
+        const postId = commentData.postId
+
+
+        try {
+            const newComment = await createComment(commentData, postId);
+            console.log(newComment);
+
+
+            setPosts((prevPosts) =>
+                prevPosts.map(post =>
+                    post._id === postId
+                        ? { ...post, comments: [newComment, ...post.comments] }
+                        : post
+                )
+            );
+
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    const [Commentvalues, commentAction, isCommentPending] = useActionState(handleSubmitComment, {
+        text: "",
+    });
+
+    const transition = { duration: 0.3, ease: "easeInOut" };
+
+    const fadeInUp = {
+        initial: { opacity: 0, y: 20 },
+        animate: { opacity: 1, y: 0 },
+        transition,
+    };
+
+    return {
+        filteredPosts,
+        postAction,
+        commentAction,
+        setSearch,
+        setSortOption,
+        expandedComments,
+        showPostForm,
+        setShowPostForm,
+        toggleComments,
+        search,
+        sortOption,
+        transition,
+        fadeInUp
+    }
+
+}
