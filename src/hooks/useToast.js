a9b1165e05a064b0563919b08cf21186e9fb1aca
Yordan Grigorov <iordan.megaparts@gmail.com>
2025-03-27 16:09:44 +0200

    Refactor info toast

--- src/hooks/useToast.js
+++ src/hooks/useToast.js
@@ -5,7 +5,7 @@ export const useToast = () => {
     return {
         success: (msg) => toast.success(msg),
         error: (msg) => toast.error(msg),
-        info: (msg) => toast.info(msg),
+        info: (msg) => toast.info(msg, { autoClose: 500 }),
         warn: (msg) => toast.warn(msg),
     };
 };
